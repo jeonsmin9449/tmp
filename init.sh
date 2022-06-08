d44477f4b7720066b75a0bb5a9db24e05f87d4f3
jeonsmin9449 <41187588+jeonsmin9449@users.noreply.github.com>
2022-06-08 10:04:31 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysql -u_USER_ -p_PW_ -P_PORT_ -e "INSTALL PLUGIN group_replication SONAME 'group_replication.so';"
+#mysql -u_USER_ -p_PW_ -P_PORT_ -e "INSTALL PLUGIN group_replication SONAME 'group_replication.so';"
 mysql -u_USER_ -p_PW_ -P_PORT_ -e "set global group_replication_local_address='_IP_:_INNODB_PORT_';"
